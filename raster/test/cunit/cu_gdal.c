2101b5562689dc67fed43be0c99fc61483aa35a9
Regina Obe <lr@pcorp.us>
2023-04-20 21:51:56 -0400

    Another try

--- raster/test/cunit/cu_gdal.c
+++ raster/test/cunit/cu_gdal.c
@@ -172,7 +172,7 @@ static void test_gdal_polygonize() {
 
 
 	gobserved = (LWGEOM *)gv[0].geom;
-	*wkt = lwgeom_to_wkt((const LWGEOM *) gobserved, WKT_EXTENDED, 2, &sz);
+	*wkt = lwgeom_to_wkt((const LWGEOM *) gobserved, WKT_EXTENDED, 2, NULL);
 	printf("observed: %s, expected: %s \n", wkt, "POLYGON((3 1,3 2,2 2,2 3,1 3,1 6,2 6,2 7,3 7,3 8,5 8,5 6,3 6,3 3,4 3,5 3,5 1,3 1))");
 	rtdealloc(wkt);
 
