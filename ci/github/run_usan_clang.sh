acee22533d5bb4b4db07a55f6c829974c0f45cee
Regina Obe <lr@pcorp.us>
2023-06-16 21:38:13 -0400

    Hardcode right version for 15

--- ci/github/run_usan_clang.sh
+++ ci/github/run_usan_clang.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
-clang --version
-CLANG_VER=11
+CLANG_FULL_VER=`clang --version`
+echo $CLANG_FULL_VER
+CLANG_VER=15
 # Enable undefined behaviour sanitizer using traps
 CFLAGS_USAN="-g3 -O0 -mtune=generic -fno-omit-frame-pointer -fsanitize=undefined,implicit-conversion -fsanitize-undefined-trap-on-error -fno-sanitize-recover=implicit-conversion"
 LDFLAGS_STD="-Wl,-Bsymbolic-functions -Wl,-z,relro"
